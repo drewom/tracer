be9853cca2e6508fa6fe38c423f53e42683cf5c7
Drew O'Malley <8580273+drewom@users.noreply.github.com>
2019-08-17 22:03:37 +0100

    Write ppm image to stdout from memory buffer.

--- tracer.c
+++ tracer.c
@@ -7,15 +7,38 @@
  * Drew O'Malley
  */
 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdint.h>
+
 int
 main (int argc, char *argv[]) { (void)argc;  (void)argv;
-	uint32_t buffer[512*512];
 
-	for (int y=0, yN=512; y<yN; ++y) {
-		for (int x=0, xN=512; x<xN; ++x) {
-			buffer[x+y*xN] = (x/4+y/4)%2 ?0xAAAAAAAA :0xCCCCCCCC;
+	enum {
+		WIDTH = 128
+	};
+	if (isatty(fileno(stdout))) {
+		puts("pipe output to the terminal");
+		return 1;
+	}
+	freopen(0, "wb", stdout);
+
+	char buffer[3*WIDTH*WIDTH];
+
+	for (int y=0, yN=WIDTH; y<yN; ++y) {
+		for (int x=0, xN=WIDTH; x<xN; ++x) {
+			int squ_type= ((x/8+y/8)*3)%2;
+			buffer[3*(x+y*xN)+0] = squ_type ?0xAA :0xCC;
+			buffer[3*(x+y*xN)+1] = squ_type ?0xAA :0xCC;
+			buffer[3*(x+y*xN)+2] = squ_type ?0xAA :0xCC;
 		}
 	}
+	printf("P6\t%d\t%d\t255\t", WIDTH, WIDTH);
+	for (int y=0, yN=WIDTH; y<yN; ++y) {
+		fwrite(buffer, sizeof(buffer), 1, stdout);
+//		putc('\n', stdout);
+	}
+	fflush(stdout);
 
 	return 0;
 }
