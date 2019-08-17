cc15bb7c71ec0f03a6d9b0d19508708ca8c786d9
Drew O'Malley <8580273+drewom@users.noreply.github.com>
2019-08-17 13:25:48 +0100

    Start on buffer creation

--- tracer.c
+++ tracer.c
@@ -2,11 +2,20 @@
  / /_____/ _ |_______ ____ ____
 / __/ __/ __ / __/ -_) __// __/
 \__/_/ /_/ |_\__/\__/_/ (_)__/
+ * "Hope is soooooooooo amazing and great!" ~ Hope O'Malley 2019
  *
  * Drew O'Malley
  */
 
 int
-main (int argc,char *argv[]) {
+main (int argc, char *argv[]) { (void)argc;  (void)argv;
+	uint32_t buffer[512*512];
+
+	for (int y=0, yN=512; y<yN; ++y) {
+		for (int x=0, xN=512; x<xN; ++x) {
+			buffer[x+y*xN] = (x/4+y/4)%2 ?0xAAAAAAAA :0xCCCCCCCC;
+		}
+	}
+
 	return 0;
 }
