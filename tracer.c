f1a611286d3cea956c5f665574a622468baaa5ea
Drew O'Malley <8580273+drewom@users.noreply.github.com>
2019-08-18 11:36:08 +0100

    Test ppm image gen.

--- tracer.c
+++ tracer.c
@@ -13,26 +13,44 @@
 
 int
 main (int argc, char *argv[]) { (void)argc;  (void)argv;
-
-	enum {
-		WIDTH = 128
-	};
+	enum { WIDTH = 120, SQUARE_SIZE = 16 };
 	if (isatty(fileno(stdout))) {
 		puts("pipe output to the terminal");
 		return 1;
 	}
 	freopen(0, "wb", stdout);
 
-	char buffer[3*WIDTH*WIDTH];
+#pragma pack(push, 1) // exact fit - no padding
+	struct { unsigned char r,g,b; } buffer[WIDTH*WIDTH];
+#pragma pack(pop)
 
 	for (int y=0, yN=WIDTH; y<yN; ++y) {
 		for (int x=0, xN=WIDTH; x<xN; ++x) {
-			int squ_type= ((x/8+y/8)*3)%2;
-			buffer[3*(x+y*xN)+0] = squ_type ?0xAA :0xCC;
-			buffer[3*(x+y*xN)+1] = squ_type ?0xAA :0xCC;
-			buffer[3*(x+y*xN)+2] = squ_type ?0xAA :0xCC;
+			int squ_type= (x/SQUARE_SIZE+y/SQUARE_SIZE)%2;
+			if (!(x/SQUARE_SIZE) && !(y/SQUARE_SIZE)){
+				buffer[x+y*xN].r = 0xCC;
+				buffer[x+y*xN].g = 0x00;
+				buffer[x+y*xN].b = 0x00;
+			} else if (x/SQUARE_SIZE == (xN-1)/SQUARE_SIZE && !(y/SQUARE_SIZE)){
+				buffer[x+y*xN].r = 0x00;
+				buffer[x+y*xN].g = 0xCC;
+				buffer[x+y*xN].b = 0x00;
+			} else if (!(x/SQUARE_SIZE) && y/SQUARE_SIZE == (yN-1)/SQUARE_SIZE){
+				buffer[x+y*xN].r = 0x00;
+				buffer[x+y*xN].g = 0x00;
+				buffer[x+y*xN].b = 0xCC;
+			} else if (x/SQUARE_SIZE == (xN-1)/SQUARE_SIZE && y/SQUARE_SIZE == (yN-1)/SQUARE_SIZE){
+				buffer[x+y*xN].r = 0x00;
+				buffer[x+y*xN].g = 0x00;
+				buffer[x+y*xN].b = 0x00;
+			} else {
+				buffer[x+y*xN].r = squ_type ?0xAA :0xCC;
+				buffer[x+y*xN].g = squ_type ?0xAA :0xCC;
+				buffer[x+y*xN].b = squ_type ?0xAA :0xCC;
+			}
 		}
 	}
+
 	printf("P6\t%d\t%d\t255\t", WIDTH, WIDTH);
 	for (int y=0, yN=WIDTH; y<yN; ++y) {
 		fwrite(buffer, sizeof(buffer), 1, stdout);
