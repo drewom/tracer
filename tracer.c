9daa448e457c9793f94b8caba2dabf2c7d3832d3
Drew O'Malley <8580273+drewom@users.noreply.github.com>
2019-08-19 18:30:27 +0100

    Add spheres to raycast

--- tracer.c
+++ tracer.c
@@ -73,13 +73,13 @@ static float
 raycast (ray_t ray, scene_t *scene, rgb24_t *out) {
 	qassert(scene); qassert(out);
 
+	/* points on ray: ray.ori + t*ray.nrm = point_on_ray where t>=0 */
 	float dist = FLT_MAX;
 	for(int p=0, pN=scene->plane_count; p<pN; ++p){
 
 		v3_t p_nrm = scene->plane_normal[p];
 		float p_dst = scene->plane_dist[p];
 
-		/* plane equ: pT*nrm + ori = 0; ray: p = ori + nrm*t */
 		float divide = v3T_mult_v3(p_nrm.T, ray.nrm);
 		if (divide < FLT_EPSILON && divide > -FLT_EPSILON) {
 			continue;
@@ -87,6 +87,42 @@ raycast (ray_t ray, scene_t *scene, rgb24_t *out) {
 		float curr = (-p_dst - v3T_mult_v3(p_nrm.T, ray.ori)) / divide;
 		if (curr > 0.f && curr < dist) {
 			*out = rgb24_from_v3(scene->material_colour[scene->plane_material[p]]);
+
+			dist = curr;
+		}
+	}
+	for(int s=0, sN=scene->sphere_count; s<sN; ++s){
+		/* sphere equation:
+		 * pT*p - s.r^2 = 0,
+		 * (ray.ori + t*ray.nrm)T*(ray.ori + t*ray.nrm) - s.r^2
+		 * solving for t:
+		 * 0 == t^2*ray.nrm.T*ray.nrm + t*2*ray.ori.T*ray.nrm + ray.ori.T*ray.ori-s.r^2
+		 *          ^a term^              ^b term^              ^c term^
+		 * t == -b +/- sqrt(b^2 -4ac) / 2a
+		 */
+		v3_t s_ori = scene->sphere_centre[s];
+		float s_rad = scene->sphere_radius[s];
+
+		ray_t r = ray;
+		r.ori = v3_add(ray.ori, v3_neg(s_ori));
+
+		/* convert r to unit circle space */
+		float a =   v3T_mult_v3(r.nrm.T, r.nrm),
+		      b = 2*v3T_mult_v3(r.ori.T, r.nrm),
+		      c =   v3T_mult_v3(r.ori.T, r.ori)-(s_rad*s_rad);
+		float sqrt_term = b*b - 4.f*a*c;
+		float _2a = 2.f*a;
+
+		if (sqrt_term < FLT_EPSILON) {
+			continue; /* ray misses sphere */
+		}
+		qassert(_2a > FLT_EPSILON || _2a < -FLT_EPSILON); /* ray normal was zero? */
+
+		/* treat spheres as one sided, if you want to draw inside then invert radius */
+		float curr = -b - sqrt(sqrt_term);
+		curr /= _2a;
+		if (curr > 0.f && curr < dist) {
+			*out = rgb24_from_v3(scene->material_colour[scene->sphere_material[s]]);
 
 			dist = curr;
 		}
@@ -104,18 +140,18 @@ main (int argc, char *argv[]) {
 
 	/* Raytracer start here */
 	scene_t scene = {};
-	scene.cam_pos = v3(0.f, 10.f, 1000.f);
+	scene.cam_pos = v3(0.f, 4.f, 10.f);
 	scene.cam_z = v3_norm_or_zero(scene.cam_pos);
 	scene.cam_x = v3_norm_or_zero(v3_cross(v3(0,1,0), scene.cam_pos));
 	scene.cam_y = v3_norm_or_zero(v3_cross(scene.cam_y, scene.cam_z));
 
-	v3_t material_colours[] = { v3(.3f,.3f,.3f), v3(.1f,.7f,.7f) };
-	v3_t plane_normals[] = {v3(0,1.f,0)};
-	float plane_dist[] = {0.f};
-	size_t plane_materials[] = {0};
-	v3_t  sphere_centres[] = {v3(0,1.f,0)};
-	float sphere_radii[] = {1.f};
-	size_t sphere_materials[] = {0};
+	v3_t material_colours[] = { v3(.3f,.3f,.2f), v3(.2f,.2f,.4f), v3(.1f,.5f,.4f), v3(.1f, .8f, .5f), v3(.6f, .1f, .5f), v3(.3f, .5f, .8f),  };
+	v3_t plane_normals[] = {v3(0,1.f,0), v3_norm_or_zero(v3(-1.f, 4.f, 0)), v3_norm_or_zero(v3(1.f, 3.f, 0))};
+	float plane_dist[] = {0.f, 4.f, 8.f};
+	size_t plane_materials[] = {0, 1, 2};
+	v3_t  sphere_centres[] = {v3(0,3.f,3),v3(5.f,0,3),v3(-4.f,3.f,6.f)};
+	float sphere_radii[] = {3.f, 2.f, 1.f};
+	size_t sphere_materials[] = {3, 4, 5};
 
 	scene.material_colour = material_colours;
 	scene.material_count = qcountof(material_colours);
@@ -130,10 +166,18 @@ main (int argc, char *argv[]) {
 	scene.sphere_material = sphere_materials;
 	scene.sphere_count = qcountof(sphere_centres);
 
+	float aspect_ratio_u = 1.f,
+	      aspect_ratio_v = 1.f;
+	if (settings.width > settings.height) {
+		aspect_ratio_u = (float)settings.width / (float)settings.height;
+	} else {
+		aspect_ratio_v = (float)settings.height / (float)settings.width;
+	}
+
 	for (int y=0, yN=settings.height; y<yN; ++y) {
-		float nearplane_v = +1.f - 2.f*((float)y/(float)yN);
+		float nearplane_v = +aspect_ratio_v - 2.f*aspect_ratio_v*((float)y/(float)yN);
 		for (int x=0, xN=settings.width; x<xN; ++x) {
-			float nearplane_u = -1.f + 2.f*((float)x/(float)xN);
+			float nearplane_u = -aspect_ratio_u + 2.f*aspect_ratio_u*((float)x/(float)xN);
 
 			ray_t ray = {};
 			ray.ori = scene.cam_pos;
@@ -183,7 +227,7 @@ static void write_image_to_ppm(FILE *to, rgb24_t *pixels, size_t width, size_t h
 static settings_t parse_options(int argc, char *argv[]) {
 	settings_t result = { 800, 600, stdout };
 #if DEBUG
-	char *_argv[] = {"", "-o", "foo.ppm", "-w", "320", "-h", "256", 0};
+	char *_argv[] = {"", "-o", "foo.ppm", "-w", "420", "-h", "256", 0};
 	argv = _argv; argc = 7;
 #endif
 
